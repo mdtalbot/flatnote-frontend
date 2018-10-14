912694cfd1fa481129eb52e546cda51ed120c8ad
Matthew Talbot <mdtalbot@gmail.com>
2018-10-14 14:02:27 -0400

    creted adaptor

---
+++ src/utilities/adapter.js
@@ -0,0 +1,36 @@
+export const fetchNotes = () => (
+  fetch("http://localhost:4000/api/v1/notes")
+);
+
+export const fetchNote = id => (
+  fetch(`http://localhost:4000/api/v1/notes/${id}`)
+);
+
+export const createNote = note => (
+  fetch(`http://localhost:4000/api/v1/notes`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify({
+      title: this.state.title,
+      body: this.state.body
+    })
+  })
+);
+
+export const updateNote = note => (
+  fetch(`http://localhost:4000/api/v1/notes/${note.id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify(
+      note
+    )
+  })
+);
+
+export const deleteNote = id => (
+  fetch(`http://localhost:4000/api/v1/notes/${id}`, { method: 'DELETE' })
+);
